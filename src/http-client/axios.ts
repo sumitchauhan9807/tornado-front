d1659a5b1f9dd56dee5d1da3439552c51fb5a8e6
Sumit chauhan <sumitchauhan9807666@gmail.com>
2026-09-05 13:12:47 +0530

    wip

--- src/http-client/axios.ts
+++ src/http-client/axios.ts
@@ -2,7 +2,7 @@ import { production } from '@/env'
 import axios from 'axios';
 
 export const baseURL = production ? 'https://backend.724voip.net/api' : "http://localhost:1337/api"
-export const assetsURL = production ? 'https://backend.724voip.net' : "http://localhost:1337"
+export const assetsURL = production ? 'https://strapi.tornadodialer.net' : "http://localhost:1337"
 export const api = axios.create({
   baseURL: baseURL
 });
