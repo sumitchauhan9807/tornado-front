f54ada0a6abce10fdb4858de4c80973e8d16e99c
Sumit chauhan <sumitchauhan9807666@gmail.com>
2026-09-22 14:09:01 +0530

    sdf

--- src/graphql/home.ts
+++ src/graphql/home.ts
@@ -30,6 +30,7 @@ export const HOME_QUERY = gql`
             content {
               ...AnimationContentFields
               ...AnimationType2Fields
+              ...AnimationType3Fields
             }
           }
         }
